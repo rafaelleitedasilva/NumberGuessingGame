2b8cdfc5b6956017cb18a02b2841fb7480b0ba43
Rafael <rafael.leite.14@hotmail.com>
2024-03-08 17:03:22 +0000

    feat: adjusting selects to get best_game and gasmes_played

--- number_guess.sh
+++ number_guess.sh
@@ -13,25 +13,28 @@ GAME(){
     echo "It's lower than that, guess again:"
     GAME
   elif [[ $GUESSED_NUMBER -lt $NUMBER ]]
+  then
     echo "It's higher than that, guess again:"
     GAME
-  elif [[ $GUESSED_NUMBER =~ ^[0-9]+$ ]]
+  elif ! [[ $GUESSED_NUMBER =~ ^[0-9]+$ ]]
+  then
     echo "That is not an integer, guess again:"
     GAME
+  else
+    echo "You are correct!"
   fi
 }
 
-USERNAME=$($PSQL "SELECT * FROM users WHERE username='$NAME';")
+USERNAME=$($PSQL "SELECT user_id FROM users WHERE username='$NAME';")
 if [[ -z $USERNAME ]]
 then
   INSERT_USERNAME=$($PSQL "INSERT INTO users(username) VALUES('$NAME');")
   USERNAME=$($PSQL "SELECT user_id FROM users WHERE username='$NAME';")
-  
 
   echo "Welcome, $NAME! It looks like this is your first time here."
 else
-  BEST_GAME=$($PSQL "SELECT number_of_guesses games WHERE number_of_guesses=MIN(number_of_guesses) AND user_id=$USERNAME;")
-  GAMES_PLAYED=$($PSQL "SELECT COUNT(games_id) FROM games WHERE user_id=$USERNAME;")
+  BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games WHERE user_id=$USERNAME;")
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id='$USERNAME';")
 
   echo Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
 fi
